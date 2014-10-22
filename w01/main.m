3c22143ad50526a157e3796cd9ec1be969b231ae
vinh-nguyen <vinhnx@zigexn.vn>
2014-10-22 19:13:25 +0900

    update newton

--- w01/main.m
+++ w01/main.m
@@ -1,4 +1,4 @@
-X = load('q1x.dat');
+( = load('q1x.dat');
 y = load('q1y.dat');
 m = size(y);
 X = [ones(m, 1) X];
@@ -8,3 +8,7 @@ theta = newton(X, y, theta);
 
 % plot
 gscatter(X(:,2), X(:,3), y, 'br', 'xo');
+hold on;
+a = -10:0.1:10;
+b = -(theta(1) + theta(2) * a) / theta(3);
+plot(a, b);
--- w01/newton.m
+++ w01/newton.m
@@ -1,6 +1,5 @@
 function theta = newton(X, y, theta)
   [m, n] = size(X);
-  theta = zeros(n, 1);
   dl = (y - sigmoid(X * theta))' * X;
   S = diag((sigmoid(X * theta) - 1) .* sigmoid(X * theta));
   h = X' * S * X;
